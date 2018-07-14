e66bf84bfe0fc3b52b1d9173d2dd050f56d60b54
Cedric Larrat <cedric.larrat@gmail.com>
2018-07-14 10:50:25 +0200

    new version

--- application.py
+++ application.py
@@ -7,13 +7,13 @@ application = Flask(__name__)
 @application.route('/core-wallet/checkversion')
 def check_version():
 	version = request.args.get('version')
-	current = '6.2.1.30'
+	current = '6.2.2.14'
 	changed = (version==current)
 	return jsonify(
 		latest=changed,
 		current_version=current,
 		change_log='Initial version',
-		tag = 'v6.2.1'
+		tag = 'v6.2.2'
 	)
 
 # run the app.
